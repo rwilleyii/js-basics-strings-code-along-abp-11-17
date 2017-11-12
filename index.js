1001484450bf24e5d0f9f2aa6e41eda5194f5644
rwilleyii <rwilley@me.com>
2017-11-12 15:16:36 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 // Write your code in this file!
 const currentUser = 'Scavenger Hunts';
 const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
-const excitedWelcomeMessage = `WELCOME TO FLATBOOK, ${currentUser}!`;
+const excitedWelcomeMessage = welcomeMessage.toUpperCase();
