71ff91e3da92c94b6ca38936bfcfd468c5981920
rwilleyii <rwilley@me.com>
2017-11-12 15:14:33 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
 // Write your code in this file!
 const currentUser = 'Scavenger Hunts';
 const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
+const excitedWelcomeMessage = `WELCOME TO FLATBOOK, ${currentUser}!`;
